88edf4716f1279b3b1c98420f8c869b20171fec6
LeeDhffff <ehdgjs1311@naver.com>
2022-03-24 01:05:38 +0900

    dd

--- sub_project_spring/src/main/java/com/multi/sub_project_spring/Login/IMemberDAO.java
+++ sub_project_spring/src/main/java/com/multi/sub_project_spring/Login/IMemberDAO.java
@@ -2,6 +2,8 @@ package com.multi.sub_project_spring.Login;
 
 import java.util.HashMap;
 
+import com.multi.sub_project_spring.Member.model.MemberVO;
+
 public interface IMemberDAO {
 
 	MemberVO loginCheck(HashMap<String, Object> map);
--- sub_project_spring/src/main/java/com/multi/sub_project_spring/Login/IMemberService.java
+++ sub_project_spring/src/main/java/com/multi/sub_project_spring/Login/IMemberService.java
@@ -2,6 +2,8 @@ package com.multi.sub_project_spring.Login;
 
 import java.util.HashMap;
 
+import com.multi.sub_project_spring.Member.model.MemberVO;
+
 public interface IMemberService {
 
 	MemberVO loginCheck(HashMap<String, Object> map);
