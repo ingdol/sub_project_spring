bd183c90fe2c92007aeb5cd309a4a3521e2c2d92
LeeDhffff <ehdgjs1311@naver.com>
2022-03-23 17:08:25 +0900

    dd

--- sub_project_spring/src/main/java/com/multi/sub_project_spring/SubProjectSpringApplication.java
+++ sub_project_spring/src/main/java/com/multi/sub_project_spring/SubProjectSpringApplication.java
@@ -1,19 +1,20 @@
 package com.multi.sub_project_spring;
 
-import com.multi.sub_project_spring.SpaceClassList.ILookUpDAO;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.multi.sub_project_spring.CreateClass.ICreateClassDAO;
 import com.multi.sub_project_spring.Member.dao.MemberDAO;
 import com.multi.sub_project_spring.QnaList.IQnaListDAO;
+import com.multi.sub_project_spring.SpaceClassList.ILookUpDAO;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.multi.sub_project_spring"})
 @MapperScan(basePackageClasses = IQnaListDAO.class)
 @MapperScan(basePackageClasses = ILookUpDAO.class)
-
+@MapperScan(basePackageClasses = ICreateClassDAO.class)
 @MapperScan(basePackageClasses = MemberDAO.class)
 
 public class SubProjectSpringApplication {
