c331aafb4e7d8d013c07ce727b651bcd28655541
YounSeoYeon <syyoon1122@gmail.com>
2022-03-24 14:09:34 +0900

    QnA상세페이지 세션 연결

--- sub_project_spring/src/main/java/com/multi/sub_project_spring/QnaList/IQnaListService.java
+++ sub_project_spring/src/main/java/com/multi/sub_project_spring/QnaList/IQnaListService.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public interface IQnaListService {
 	
 	ArrayList<QnaListVO> qnaList();
-	QnaListVO qnadetailview(int hostqnaNo);
+	QnaListVO qnadetailview(int hostqnaNo, String memNick);
 	void qnainsert(QnaListVO qna);
 	void qnadelete(int hostqnaNo);
 	void qnaupdate(QnaListVO qna);
