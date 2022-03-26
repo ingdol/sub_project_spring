91d4ceb43ca8f85f6fb77a1401e0b9c20b567e21
LeeDhffff <ehdgjs1311@naver.com>
2022-03-26 20:14:37 +0900

    호스트 권한 있는지 없는지 체크

--- sub_project_spring/src/main/java/com/multi/sub_project_spring/CreateClass/CreateController.java
+++ sub_project_spring/src/main/java/com/multi/sub_project_spring/CreateClass/CreateController.java
@@ -1,10 +1,17 @@
 package com.multi.sub_project_spring.CreateClass;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.multi.sub_project_spring.Member.model.MemberVO;
 import com.multi.sub_project_spring.SpaceClassList.ClassVO;
 import com.multi.sub_project_spring.SpaceClassList.SpaceVO;
 
@@ -15,6 +22,8 @@ public class CreateController {
 	CreateClassService service;
 	@Autowired
 	CreateSpaceService service2;
+	
+	String memNick = "";
 	
 	@RequestMapping(value="/Class/insert")
 	public void insertClass( ClassVO cl) {		
@@ -29,4 +38,21 @@ public class CreateController {
 
 		service2.insertSpace(sp);	 
 	}
+	
+    @RequestMapping("SessionFourList")
+    public void sessionNickname(@RequestBody HashMap<String, Object> requestData) {
+        
+    	memNick = (String) requestData.get("sNick");
+    	System.out.println(memNick);
+    }
+
+    @RequestMapping("HostCheck")
+    public int HostCheck(HttpSession session) {
+        
+    	ArrayList<HostVO> cmList =  service.HostCheck(memNick);
+       int check = cmList.size();
+        
+        
+        return check;
+    }
 }
--- sub_project_spring/src/main/java/com/multi/sub_project_spring/CreateClass/ICreateClassDAO.java
+++ sub_project_spring/src/main/java/com/multi/sub_project_spring/CreateClass/ICreateClassDAO.java
@@ -1,7 +1,11 @@
 package com.multi.sub_project_spring.CreateClass;
 
+import java.util.ArrayList;
+
+import com.multi.sub_project_spring.Member.model.MemberVO;
 import com.multi.sub_project_spring.SpaceClassList.ClassVO;
 
 public interface ICreateClassDAO {
 	void insertClass(ClassVO cl);
+	ArrayList<HostVO> HostCheck(String memNick);
 }
